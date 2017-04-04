eedc459d7d68ad36dc6cc95e69e083d92f056884
Manuel Alonso Rodrígez <Manuel Alonso Rodrgez>
2017-04-04 17:45:07 +0200

    Boton enviar y editText creado

--- app/src/main/java/com/example/galle/comunicacionentrefragments/MainActivity.java
+++ app/src/main/java/com/example/galle/comunicacionentrefragments/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.galle.comunicacionentrefragments;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText textonEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textonEnviar = (EditText)findViewById(R.id.texto);
     }
+
+
 }
